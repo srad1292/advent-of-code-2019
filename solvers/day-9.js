dd4e9d6dedb3fa722c485953f5d5d24da74a19e5
Sam Radford <smr1292@yahoo.com>
2023-08-30 11:29:16 -0400

    Day 9 Part One Samples Working -> need to get actual solution now

---
+++ solvers/day-9.js
@@ -0,0 +1,248 @@
+const SensorBoost = {
+    addOP: 1,
+    multOP: 2,
+    saveOP: 3,
+    outputOP: 4,
+    jumpIfTrueOP: 5,
+    jumpIfFalseOP: 6,
+    lessThanOP: 7,
+    equalsOP: 8,
+    relBaseOP: 9,
+    endOP: 99,
+    posMode: 0,
+    imdMode: 1,
+    relMode: 2,
+    maxParams: 2,
+
+
+    solvePartOne: (state, input) => {
+        return SensorBoost.intCode(state,[input, input], 0, 0, false);
+    },
+    intCode: (state, input, instructionPointer, inputIndex, pauseOnOutput) => {
+        startingState = [...state];
+        let currentIndex = instructionPointer;
+        let statusOK = true;
+        let op = 0;
+        let opAndParams = [];
+        let valueA = 0;
+        let valueB = 0;
+        let storeValue = 0;
+        let destination = 0;
+        let output = [];
+        let halted = false;
+        let relativeBase = 0;
+        let getRWResult = {};
+
+        try {
+            while(statusOK && currentIndex < state.length) {
+                opAndParams = SensorBoost.opcodeToList(state[currentIndex]);
+                op = opAndParams[0];
+                if(op === SensorBoost.endOP) {
+                    halted = true;
+                    break;
+                } else if(op === SensorBoost.addOP || op === SensorBoost.multOP || op === SensorBoost.equalsOP || op === SensorBoost.lessThanOP) {
+                    getRWResult = SensorBoost.getReadParamValue(state, currentIndex, relativeBase, 1, opAndParams);
+                    state = getRWResult.state;
+                    valueA = getRWResult.value;
+
+                    getRWResult = SensorBoost.getReadParamValue(state, currentIndex, relativeBase, 2, opAndParams);
+                    state = getRWResult.state;
+                    valueB = getRWResult.value;
+
+
+                    destination = state[currentIndex+3];
+
+                    if(op === SensorBoost.addOP) {
+                        storeValue = valueA+valueB;
+                    } else if(op === SensorBoost.multOP) {
+                        storeValue = valueA*valueB;
+                    } else if(op === SensorBoost.equalsOP) {
+                        storeValue = valueA === valueB ? 1 : 0;
+                    } else if(op === SensorBoost.lessThanOP) {
+                        storeValue = valueA < valueB ? 1 : 0;
+                    }
+                    state[destination] = storeValue;
+                    currentIndex+=4;
+                    // console.log({op: "Add/Mul/Eq/Less", destination, storeValue, currentIndex, relativeBase});
+
+                } else if(op === SensorBoost.saveOP) {
+                    destination = state[currentIndex+1];
+                    state[destination] = input[inputIndex];
+                    if(inputIndex<input.length-1) {
+                        inputIndex++;
+                    }
+                    currentIndex+=2;
+                } else if(op === SensorBoost.outputOP) {
+                    getRWResult = SensorBoost.getReadParamValue(state, currentIndex, relativeBase, 1, opAndParams);
+                    state = getRWResult.state;
+                    valueA = getRWResult.value;
+
+                    output.push(valueA);
+                    currentIndex+=2;
+                    if(pauseOnOutput) { break };
+                } else if(op === SensorBoost.jumpIfTrueOP || op === SensorBoost.jumpIfFalseOP) { 
+                    getRWResult = SensorBoost.getReadParamValue(state, currentIndex, relativeBase, 1, opAndParams);
+                    state = getRWResult.state;
+                    valueA = getRWResult.value;
+
+                    getRWResult = SensorBoost.getReadParamValue(state, currentIndex, relativeBase, 2, opAndParams);
+                    state = getRWResult.state;
+                    destination = getRWResult.value;
+
+                    let shouldJump = (op===SensorBoost.jumpIfTrueOP&&valueA!==0) || (op===SensorBoost.jumpIfFalseOP&&valueA===0);
+                    currentIndex = shouldJump ? destination : currentIndex+3;
+
+                } else if(op === SensorBoost.relBaseOP) {
+                    getRWResult = SensorBoost.getReadParamValue(state, currentIndex, relativeBase, 1, opAndParams);
+                    state = getRWResult.state;
+                    valueA = getRWResult.value;
+                    
+                    relativeBase += valueA;
+                    currentIndex+=2;
+                } else {
+                    break;
+                }
+            }
+            return {state, output, halted, instructionPointer: currentIndex, inputIndex};
+        } catch(e) {
+            console.log("Error while creating alarm program state");
+            console.log(e);
+            console.log({state, output, halted, instructionPointer: currentIndex});
+            return {state, output, halted, instructionPointer: currentIndex};
+        }
+    },
+    expandMemory(state, idx) {
+        let end = idx-(state.length-1);
+        for(let addIdx = 0; addIdx < end; addIdx++) { 
+            state.push(0);
+        }
+        return state;
+    },
+    getReadParamValue: (state, currentIndex, relativeBase, paramNum, opAndParams) => {
+        let offset = opAndParams[paramNum] == SensorBoost.relMode ? relativeBase : 0;
+        let targetIndex = opAndParams[paramNum] == SensorBoost.imdMode ? currentIndex+paramNum : state[currentIndex+paramNum]+offset;
+        if(targetIndex >= state.length) {
+            state = SensorBoost.expandMemory(state, targetIndex);
+        }
+        return {state, value: state[targetIndex]};
+    },
+    opcodeToList: (opcode) => {
+        let result = [];
+        // Get two digit operation
+        let op = (opcode%10)+(Math.floor(opcode/10)%10*10);
+        result.push(op);
+        // Get parameter mode
+        opcode = Math.floor(opcode/100);
+        while(opcode > 0) {
+            result.push(opcode%10);
+            opcode = Math.floor(opcode/10);
+        }
+        // In the case all param modes are given, these will not be seen and thus don't matter if they are there
+        // In the case all param modes are not given, this will allow us to get them without worrying about accessing out of bounds index
+        for(let idx = 0; idx < SensorBoost.maxParams; idx++) {
+            result.push(0);
+        }
+
+        return result;
+    },
+    findNounAndVerb: (memory, target, min, max) => {
+        let freshMemory = [];
+        let currentSolution = [];
+        let solutionNoun = 0;
+        let solutionVerb = 0;
+        let found = false;
+        for(let noun=min; noun <= max; noun++) {
+            for(let verb=min; verb <= max; verb++) {
+                freshMemory = [...memory];
+                freshMemory[1] = noun;
+                freshMemory[2] = verb;
+                currentSolution = SensorBoost.solve(freshMemory);
+                if(currentSolution[0] === target) {
+                    solutionNoun = noun;
+                    solutionVerb = verb;
+                    found = true;
+                    break;
+                }
+            }
+        }
+        return found ? [solutionNoun, solutionVerb] : [-1, 1];
+
+    },
+    buildPermutations: (input) => {
+        let permutations = [];
+        if (input.length === 0) {
+            return [[]];
+        }
+        for (let i = 0; i < input.length; i++) {
+            let rest = SensorBoost.buildPermutations(input.slice(0, i).concat(input.slice(i + 1)));
+            for (let j = 0; j < rest.length; j++) {
+                permutations.push([input[i]].concat(rest[j]));
+            }
+        }
+        return permutations;
+    },
+    day7SolvePartOne: (state, input, phaseSettings) => {
+        const permutations = SensorBoost.buildPermutations(phaseSettings);
+        let maxSignal = -1;
+        let nextInput = input;
+        let output;
+        permutations.forEach(permutation => {
+            nextInput = input;
+            permutation.forEach(phase => {
+                output = SensorBoost.intCode(state,[phase,nextInput], 0, 0, false).output;
+                nextInput = output[output.length-1];
+            });
+            maxSignal = Math.max(maxSignal, nextInput);
+        });
+        return maxSignal;
+    },
+    day7SolvePartTwo: (state, input, phaseSettings, outputAmpIndex) => {
+        const permutations = SensorBoost.buildPermutations(phaseSettings);
+        let maxSignal = -1;
+        let nextInput = input;
+        let output;
+        let amplifiers = [];
+        let halted = false;
+        let amplifierIndex = 0;
+        let amplifier;
+        permutations.forEach(permutation => {
+            nextInput = input;
+            amplifiers = [];
+            halted = false;
+            amplifierIndex = 0;
+            permutation.forEach(phase => {
+                amplifiers.push({
+                    state: [...state],
+                    instructionPointer: 0,
+                    output: -1,
+                    input: nextInput,
+                    phase: phase,
+                    inputIndex: 0
+                });
+            });
+            while(!halted) {
+                amplifier = amplifiers[amplifierIndex];
+                output = SensorBoost.intCode(amplifier.state, [amplifier.phase, amplifier.input], amplifier.instructionPointer, amplifier.inputIndex, true);
+                amplifier.state = [...output.state];
+                amplifier.instructionPointer = output.instructionPointer;
+                if(output.output.length > 0) {
+                    amplifier.output = output.output[output.output.length-1];
+                }
+                amplifier.inputIndex = output.inputIndex;
+
+                if(amplifierIndex === outputAmpIndex) {
+                    maxSignal = Math.max(maxSignal, amplifier.output);
+                }
+
+                halted = output.halted;
+
+                amplifierIndex = amplifierIndex === amplifiers.length-1 ? 0 : amplifierIndex+1;
+                amplifiers[amplifierIndex].input = amplifier.output;
+            }
+            
+        });
+        return maxSignal;
+    },
+};
+
+module.exports = SensorBoost;
---
+++ tests/day-9.test.js
@@ -0,0 +1,174 @@
+const SensorBoost = require('../solvers/day-9');
+
+
+describe("Day 7 - Build permutation", () => {
+    test("Three numbers returns 6 permutations", () => {
+        let input = [0,1,2];
+        let expected = 6; // 3*2*1
+        let actual = SensorBoost.buildPermutations(input).length;
+        expect(actual).toEqual(expected);
+    });
+
+    test("Four numbers returns 24 permutations", () => {
+        let input = [0,1,2,3];
+        let expected = 24; // 4*3*2*1
+        let actual = SensorBoost.buildPermutations(input).length;
+        expect(actual).toEqual(expected);
+    });
+
+    test("Five numbers returns 120 permutations", () => {
+        let input = [0,1,2,3,4];
+        let expected = 120; // 5*4*3*2*1
+        let actual = SensorBoost.buildPermutations(input).length;
+        expect(actual).toEqual(expected);
+    });
+
+    test("Permutations are different - Part A", () => {
+        let input = [0,1];
+        let expected = [0,1];
+        let actual = SensorBoost.buildPermutations(input)[0];
+        expect(actual).toStrictEqual(expected);
+    });
+
+    test("Permutations are different - Part B", () => {
+        let input = [0,1];
+        let expected = [1,0];
+        let actual = SensorBoost.buildPermutations(input)[1];
+        expect(actual).toStrictEqual(expected);
+    });
+});
+
+describe("Day 7 - Find Max Signal", () => {
+    test("Sample One", () => {
+        let startingState = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0];
+        let input = 0;
+        let phaseSettings = [0,1,2,3,4];
+        let actual = SensorBoost.day7SolvePartOne(startingState,input,phaseSettings);
+        let expected = 43210;
+        expect(actual).toEqual(expected);
+    });
+
+    test("Sample Two", () => {
+        let startingState = [3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0];
+        let input = 0;
+        let phaseSettings = [0,1,2,3,4];
+        let actual = SensorBoost.day7SolvePartOne(startingState,input,phaseSettings);
+        let expected = 54321;
+        expect(actual).toEqual(expected);
+    });
+
+    test("Sample Three", () => {
+        let startingState = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0];
+        let input = 0;
+        let phaseSettings = [0,1,2,3,4];
+        let actual = SensorBoost.day7SolvePartOne(startingState,input,phaseSettings);
+        let expected = 65210;
+        expect(actual).toEqual(expected);
+    });
+
+    test("Actual Part One", () => {
+        let startingState = [3,8,1001,8,10,8,105,1,0,0,21,34,51,76,101,114,195,276,357,438,99999,3,9,
+            1001,9,3,9,1002,9,3,9,4,9,99,3,9,101,4,9,9,102,4,9,9,1001,9,5,9,4,9,99,3,9,1002,9,4,9,101,3,9,9,
+            102,5,9,9,1001,9,2,9,1002,9,2,9,4,9,99,3,9,1001,9,3,9,102,2,9,9,101,4,9,9,102,3,9,9,101,2,9,9,4,9,
+            99,3,9,102,2,9,9,101,4,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,
+            9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,
+            9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,
+            4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,
+            9,1001,9,2,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,
+            9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,
+            99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,
+            2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,
+            3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,
+            4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99
+        ];
+        let input = 0;
+        let phaseSettings = [0,1,2,3,4];
+        let actual = SensorBoost.day7SolvePartOne(startingState,input,phaseSettings);
+        let expected = 38834;
+        expect(actual).toEqual(expected);
+    });
+});
+
+describe("Day 7 - Find Max Signal Circuit Mode", () => {
+    test("Sample One", () => {
+        let startingState = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5];
+        let input = 0;
+        let phaseSettings = [9,8,7,6,5];
+        let outputAmpIndex = 4;
+        let actual = SensorBoost.day7SolvePartTwo(startingState,input,phaseSettings, outputAmpIndex);
+        let expected = 139629729;
+        expect(actual).toEqual(expected);
+    });
+
+    test("Sample Two", () => {
+        let startingState = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+            -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+            53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10
+        ];
+        let input = 0;
+        let phaseSettings = [9,8,7,6,5];
+        let outputAmpIndex = 4;
+        let actual = SensorBoost.day7SolvePartTwo(startingState,input,phaseSettings, outputAmpIndex);
+        let expected = 18216;
+        expect(actual).toEqual(expected);
+    });
+
+    test("Actual Part One", () => {
+        let startingState = [3,8,1001,8,10,8,105,1,0,0,21,34,51,76,101,114,195,276,357,438,99999,3,9,
+            1001,9,3,9,1002,9,3,9,4,9,99,3,9,101,4,9,9,102,4,9,9,1001,9,5,9,4,9,99,3,9,1002,9,4,9,101,3,9,9,
+            102,5,9,9,1001,9,2,9,1002,9,2,9,4,9,99,3,9,1001,9,3,9,102,2,9,9,101,4,9,9,102,3,9,9,101,2,9,9,4,9,
+            99,3,9,102,2,9,9,101,4,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,
+            9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,
+            9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,
+            4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,
+            9,1001,9,2,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,
+            9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,
+            99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,
+            2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,
+            3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,
+            4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99
+        ];
+        let input = 0;
+        let phaseSettings = [9,8,7,6,5];
+        let outputAmpIndex = 4;
+        let actual = SensorBoost.day7SolvePartTwo(startingState,input,phaseSettings, outputAmpIndex);
+        let expected = 69113332;
+        expect(actual).toEqual(expected);
+    });
+});
+
+describe("Today - Expand Memory", () => {
+    test("Sample One", () => {
+        let state = [0,2,3];
+        let indexToGoTo = 9;
+        let actual = SensorBoost.expandMemory(state, indexToGoTo).length;
+        let expected = indexToGoTo+1;
+        expect(actual).toEqual(expected);
+    });
+});
+
+describe("Solve Part One", () => {
+    test("Sample One", () => {
+        let startingState = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99];
+        let output = SensorBoost.solvePartOne(startingState, 0);
+        let expected = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99];
+        let actual = output.output;
+        expect(actual).toStrictEqual(expected);
+    });
+    
+    test("Sample Two", () => {
+        let startingState = [1102,34915192,34915192,7,4,7,99,0];
+        let output = SensorBoost.solvePartOne(startingState, 0);
+        let expected = 16;
+        let actual = (output.output[0]).toString().length;
+        expect(actual).toEqual(expected);
+    });
+    
+    test("Sample Three", () => {
+        let startingState = [104,1125899906842624,99];
+        let output = SensorBoost.solvePartOne(startingState, 0);
+        let expected = 1125899906842624;
+        let actual = output.output[0];
+        expect(actual).toEqual(expected);
+    });
+});
