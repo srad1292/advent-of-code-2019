0e3539aaf9d6831d6e962e33005f613d66defd79
Sam Radford <smr1292@yahoo.com>
2023-09-05 10:50:14 -0400

    Day 13 - Part 1

---
+++ solvers/day-13.js
@@ -0,0 +1,374 @@
+const Tile = {
+    Empty: 0,
+    Wall: 1,
+    Block: 2,
+    HorPaddle: 3,
+    Ball: 4
+}
+
+const OutputType = {
+    XPos: 0,
+    YPos: 1,
+    TileId: 2,
+}
+
+const CarePackage = {
+    addOP: 1,
+    multOP: 2,
+    saveOP: 3,
+    outputOP: 4,
+    jumpIfTrueOP: 5,
+    jumpIfFalseOP: 6,
+    lessThanOP: 7,
+    equalsOP: 8,
+    relBaseOP: 9,
+    endOP: 99,
+    posMode: 0,
+    imdMode: 1,
+    relMode: 2,
+    maxParams: 2,
+
+    debugPart1: false,
+    debugPart2: false,
+    debugIntCode: false,
+
+    solvePartOne: (state) => {
+        // key = row-col  val = {tileType: Tile}
+        let position = [0,0];
+        let grid = {};
+        grid[`${position[0]}-${position[1]}`] = {tileType: Tile.Empty};
+        let outType = OutputType.XPos;
+        let halted = false;
+
+        let arcade = {
+            state: [...state],
+            instructionPointer: 0,
+            output: -1,
+            input: 0,
+            phase: 0,
+            inputIndex: 0,
+            relativeBase: 0
+        };
+
+        let blockCount = 0;
+
+        while(!halted) {
+            output = CarePackage.intCode(arcade.state, [arcade.input, arcade.input], arcade.instructionPointer, arcade.inputIndex, true, arcade.relativeBase);
+            arcade.state = [...output.state];
+            arcade.instructionPointer = output.instructionPointer;
+            arcade.relativeBase = output.relativeBase;
+            if(output.output.length > 0) {
+                arcade.output = output.output[0];
+            }
+            arcade.inputIndex = output.inputIndex;
+
+            halted = output.halted;
+
+            if(!halted) {
+                if(outType === OutputType.XPos) {
+                    position[0] = arcade.output;
+                    outType++;
+                } else if(outType === OutputType.YPos) {
+                    position[1] = arcade.output;
+                    outType++;
+                } else {
+                    if(grid[`${position[0]}-${position[1]}`] === undefined) {
+                        grid[`${position[0]}-${position[1]}`] = {tileType: Tile.Empty};           
+                    } 
+                    if(grid[`${position[0]}-${position[1]}`].tileType === Tile.Block && arcade.output !== Tile.Block) {
+                        blockCount = Math.max(0, blockCount-1);           
+                    } else if(grid[`${position[0]}-${position[1]}`].tileType !== Tile.Block && arcade.output === Tile.Block) {
+                        blockCount++;
+                    }
+                    grid[`${position[0]}-${position[1]}`].tileType = arcade.output;
+                    outType = OutputType.XPos;
+                }
+            }
+        }
+        return blockCount;
+    },
+    intCode: (state, input, instructionPointer, inputIndex, pauseOnOutput, relativeBase) => {
+        startingState = [...state];
+        let currentIndex = instructionPointer;
+        let statusOK = true;
+        let op = 0;
+        let opAndParams = [];
+        let valueA = 0;
+        let valueB = 0;
+        let storeValue = 0;
+        let destination = 0;
+        let output = [];
+        let halted = false;
+        let getRWResult = {};
+
+        try {
+            while(statusOK && instructionPointer !== undefined && currentIndex < state.length) {
+                opAndParams = CarePackage.opcodeToList(state[currentIndex]);
+                op = opAndParams[0];
+                if(CarePackage.debugIntCode) {
+                    CarePackage.printOp(opAndParams, currentIndex);
+                }
+                if(op === CarePackage.endOP) {
+                    halted = true;
+                    break;
+                } else if(op === CarePackage.addOP || op === CarePackage.multOP || op === CarePackage.equalsOP || op === CarePackage.lessThanOP) {
+                    getRWResult = CarePackage.getReadParamValue(state, currentIndex, relativeBase, 1, opAndParams);
+                    state = getRWResult.state;
+                    valueA = getRWResult.value;
+
+                    getRWResult = CarePackage.getReadParamValue(state, currentIndex, relativeBase, 2, opAndParams);
+                    state = getRWResult.state;
+                    valueB = getRWResult.value;
+
+
+                    getRWResult = CarePackage.getWriteParamValue(state, currentIndex, relativeBase, 3, opAndParams);
+                    state = getRWResult.state;
+                    destination = getRWResult.value;
+
+
+                    if(op === CarePackage.addOP) {
+                        storeValue = valueA+valueB;
+                    } else if(op === CarePackage.multOP) {
+                        storeValue = valueA*valueB;
+                    } else if(op === CarePackage.equalsOP) {
+                        storeValue = valueA === valueB ? 1 : 0;
+                    } else if(op === CarePackage.lessThanOP) {
+                        storeValue = valueA < valueB ? 1 : 0;
+                    }
+                    state[destination] = storeValue;
+                    currentIndex+=4;
+
+                } else if(op === CarePackage.saveOP) {
+                    
+                    getRWResult = CarePackage.getWriteParamValue(state, currentIndex, relativeBase, 1, opAndParams);
+                    state = getRWResult.state;
+                    destination = getRWResult.value;
+                    
+                    if(CarePackage.debugIntCode) {
+                        console.log(`Saving ${input[inputIndex]} into location ${destination}`);
+                    }
+                    state[destination] = input[inputIndex];
+                    if(inputIndex<input.length-1) {
+                        inputIndex++;
+                    }
+                    currentIndex+=2;
+                } else if(op === CarePackage.outputOP) {
+                    getRWResult = CarePackage.getReadParamValue(state, currentIndex, relativeBase, 1, opAndParams);
+                    state = getRWResult.state;
+                    valueA = getRWResult.value;
+
+                    output.push(valueA);
+                    currentIndex+=2;
+                    if(pauseOnOutput) { break };
+                } else if(op === CarePackage.jumpIfTrueOP || op === CarePackage.jumpIfFalseOP) { 
+                    getRWResult = CarePackage.getReadParamValue(state, currentIndex, relativeBase, 1, opAndParams);
+                    state = getRWResult.state;
+                    valueA = getRWResult.value;
+
+                    getRWResult = CarePackage.getReadParamValue(state, currentIndex, relativeBase, 2, opAndParams);
+                    state = getRWResult.state;
+                    destination = getRWResult.value;
+
+                    let shouldJump = (op===CarePackage.jumpIfTrueOP&&valueA!==0) || (op===CarePackage.jumpIfFalseOP&&valueA===0);
+                    currentIndex = shouldJump ? destination : currentIndex+3;
+                    if(CarePackage.debugIntCode) {
+                        console.log(`Checking against value ${valueA} -- Should jump ${shouldJump} -- Moving to ${currentIndex}`);
+                    }
+
+                } else if(op === CarePackage.relBaseOP) {
+                    getRWResult = CarePackage.getReadParamValue(state, currentIndex, relativeBase, 1, opAndParams);
+                    state = getRWResult.state;
+                    valueA = getRWResult.value;
+                    
+                    relativeBase += valueA;
+                    currentIndex+=2;
+                } else {
+                    break;
+                }
+            }
+            return {state, output, halted, instructionPointer: currentIndex, inputIndex, relativeBase};
+        } catch(e) {
+            console.log("Error while creating alarm program state");
+            console.log(e);
+            console.log({state, output, halted, instructionPointer: currentIndex});
+            return {state, output, halted, instructionPointer: currentIndex, relativeBase};
+        }
+    },
+    printOp: (opAndParams, currentIndex) => {
+        let op = CarePackage.opToName(opAndParams[0]);
+        console.log(`Instruction ${currentIndex}: ${op}`);
+    },
+    opToName: (op) => {
+        if(op === CarePackage.addOP) {
+            return 'Add';
+        } else if(op === CarePackage.multOP) {
+            return 'Multiply';
+        } else if(op === CarePackage.saveOP) {
+            return 'Save';
+        } else if(op === CarePackage.outputOP) {
+            return 'Output';
+        } else if(op === CarePackage.jumpIfTrueOP) {
+            return 'Jump If True';
+        } else if(op === CarePackage.jumpIfFalseOP) {
+            return 'Jump If False';
+        } else if(op === CarePackage.lessThanOP) {
+            return 'Less Than';
+        }  else if(op === CarePackage.equalsOP) {
+            return 'Equals';
+        } else if(op === CarePackage.relBaseOP) {
+            return 'Change Relative Base';
+        } else if(op === CarePackage.endOP) {
+            return 'Halt';
+        }
+
+        return 'Unknown OP';
+    },
+    expandMemory(state, idx) {
+        let end = idx-(state.length-1);
+        for(let addIdx = 0; addIdx < end; addIdx++) { 
+            state.push(0);
+        }
+        return state;
+    },
+    getReadParamValue: (state, currentIndex, relativeBase, paramNum, opAndParams) => {
+        let offset = opAndParams[paramNum] == CarePackage.relMode ? relativeBase : 0;
+        let targetIndex = opAndParams[paramNum] == CarePackage.imdMode ? currentIndex+paramNum : state[currentIndex+paramNum]+offset;
+        if(targetIndex >= state.length) {
+            state = CarePackage.expandMemory(state, targetIndex);
+        }
+        return {state, value: state[targetIndex]};
+    },
+    getWriteParamValue: (state, currentIndex, relativeBase, paramNum, opAndParams) => {
+        let offset = opAndParams[paramNum] == CarePackage.relMode ? relativeBase : 0;
+        let targetIndex = opAndParams[paramNum] == CarePackage.relMode ?  state[currentIndex+paramNum]+offset : state[currentIndex+paramNum];
+        if(targetIndex >= state.length) {
+            state = CarePackage.expandMemory(state, targetIndex);
+        }
+        return {state, value: targetIndex};
+    },
+    opcodeToList: (opcode) => {
+        let result = [];
+        // Get two digit operation
+        let op = (opcode%10)+(Math.floor(opcode/10)%10*10);
+        result.push(op);
+        // Get parameter mode
+        opcode = Math.floor(opcode/100);
+        while(opcode > 0) {
+            result.push(opcode%10);
+            opcode = Math.floor(opcode/10);
+        }
+        // In the case all param modes are given, these will not be seen and thus don't matter if they are there
+        // In the case all param modes are not given, this will allow us to get them without worrying about accessing out of bounds index
+        for(let idx = 0; idx < CarePackage.maxParams; idx++) {
+            result.push(0);
+        }
+
+        return result;
+    },
+    findNounAndVerb: (memory, target, min, max) => {
+        let freshMemory = [];
+        let currentSolution = [];
+        let solutionNoun = 0;
+        let solutionVerb = 0;
+        let found = false;
+        for(let noun=min; noun <= max; noun++) {
+            for(let verb=min; verb <= max; verb++) {
+                freshMemory = [...memory];
+                freshMemory[1] = noun;
+                freshMemory[2] = verb;
+                currentSolution = CarePackage.solve(freshMemory);
+                if(currentSolution[0] === target) {
+                    solutionNoun = noun;
+                    solutionVerb = verb;
+                    found = true;
+                    break;
+                }
+            }
+        }
+        return found ? [solutionNoun, solutionVerb] : [-1, 1];
+
+    },
+    buildPermutations: (input) => {
+        let permutations = [];
+        if (input.length === 0) {
+            return [[]];
+        }
+        for (let i = 0; i < input.length; i++) {
+            let rest = CarePackage.buildPermutations(input.slice(0, i).concat(input.slice(i + 1)));
+            for (let j = 0; j < rest.length; j++) {
+                permutations.push([input[i]].concat(rest[j]));
+            }
+        }
+        return permutations;
+    },
+    day9SolvePartOne: (state, input) => {
+        return CarePackage.intCode(state,[input, input], 0, 0, false,0);
+    },
+    day9SolvePartTwo: (state, input) => {
+        return CarePackage.intCode(state,[input, input], 0, 0, false,0);
+    },
+    day7SolvePartOne: (state, input, phaseSettings) => {
+        const permutations = CarePackage.buildPermutations(phaseSettings);
+        let maxSignal = -1;
+        let nextInput = input;
+        let output;
+        permutations.forEach(permutation => {
+            nextInput = input;
+            permutation.forEach(phase => {
+                output = CarePackage.intCode(state,[phase,nextInput], 0, 0, false, 0).output;
+                nextInput = output[output.length-1];
+            });
+            maxSignal = Math.max(maxSignal, nextInput);
+        });
+        return maxSignal;
+    },
+    day7SolvePartTwo: (state, input, phaseSettings, outputAmpIndex) => {
+        const permutations = CarePackage.buildPermutations(phaseSettings);
+        let maxSignal = -1;
+        let nextInput = input;
+        let output;
+        let amplifiers = [];
+        let halted = false;
+        let amplifierIndex = 0;
+        let amplifier;
+        permutations.forEach(permutation => {
+            nextInput = input;
+            amplifiers = [];
+            halted = false;
+            amplifierIndex = 0;
+            permutation.forEach(phase => {
+                amplifiers.push({
+                    state: [...state],
+                    instructionPointer: 0,
+                    output: -1,
+                    input: nextInput,
+                    phase: phase,
+                    inputIndex: 0
+                });
+            });
+            while(!halted) {
+                amplifier = amplifiers[amplifierIndex];
+                output = CarePackage.intCode(amplifier.state, [amplifier.phase, amplifier.input], amplifier.instructionPointer, amplifier.inputIndex, true, 0);
+                amplifier.state = [...output.state];
+                amplifier.instructionPointer = output.instructionPointer;
+                if(output.output.length > 0) {
+                    amplifier.output = output.output[output.output.length-1];
+                }
+                amplifier.inputIndex = output.inputIndex;
+
+                if(amplifierIndex === outputAmpIndex) {
+                    maxSignal = Math.max(maxSignal, amplifier.output);
+                }
+
+                halted = output.halted;
+
+                amplifierIndex = amplifierIndex === amplifiers.length-1 ? 0 : amplifierIndex+1;
+                amplifiers[amplifierIndex].input = amplifier.output;
+            }
+            
+        });
+        return maxSignal;
+    },
+};
+
+module.exports = CarePackage;
