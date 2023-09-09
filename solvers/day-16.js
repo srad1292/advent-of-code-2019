002dab0176671c2fef88827142461616ca5a2c31
Sam Radford <smr1292@yahoo.com>
2023-09-09 16:41:32 -0400

    Started part 2 but can see the simple way will be too slow.  Dont have time to work on real solution now

--- solvers/day-16.js
+++ solvers/day-16.js
@@ -3,6 +3,21 @@ const FFT = {
         let input = FFT.inputStrToArr(str);
         return FFT.calculateOutput(input, pattern, phases).slice(0,8);
     },
+    solvePartTwo: (str, pattern, phases) => {
+        let bigStr = '';
+        for(let idx = 0; idx < 10000; idx++) {
+            bigStr = `${bigStr}${str}`
+        }
+        console.log("Big str length: " + bigStr.length);
+        let singleInput = FFT.inputStrToArr(bigStr);
+        // let fullInput = [];
+        // for(let idx = 0; idx < 10000; idx++) {
+            // fullInput = [...fullInput,...singleInput];
+        // }
+        let offset = parseInt(singleInput.slice(0,7).join(""));
+        console.log("offset: " + offset);
+        return FFT.calculateOutput(singleInput, pattern, phases).slice(offset,offset+8);
+    },
     inputStrToArr: (str) => {
         return str.split("").map(val => parseInt(val));
     },
