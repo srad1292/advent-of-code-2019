331a1bbb631cfce0f22e2f0e4d321c9f2fd8f5a5
Sam Radford <smr1292@yahoo.com>
2023-09-12 16:37:31 -0400

    Day 16 pt 2

--- solvers/day-16.js
+++ solvers/day-16.js
@@ -4,19 +4,24 @@ const FFT = {
         return FFT.calculateOutput(input, pattern, phases).slice(0,8);
     },
     solvePartTwo: (str, pattern, phases) => {
+        // 82994322
         let bigStr = '';
         for(let idx = 0; idx < 10000; idx++) {
             bigStr = `${bigStr}${str}`
         }
-        console.log("Big str length: " + bigStr.length);
-        let singleInput = FFT.inputStrToArr(bigStr);
-        // let fullInput = [];
-        // for(let idx = 0; idx < 10000; idx++) {
-            // fullInput = [...fullInput,...singleInput];
-        // }
-        let offset = parseInt(singleInput.slice(0,7).join(""));
-        console.log("offset: " + offset);
-        return FFT.calculateOutput(singleInput, pattern, phases).slice(offset,offset+8);
+
+        let input = FFT.inputStrToArr(bigStr);
+        let offset = parseInt(input.slice(0,7).join(""));
+        // Offset is after halfway so we can remove anything before the offset
+        let reduced = input.slice(offset);
+
+        for (let phase = 1; phase <= 100; phase++) {
+            for (let i = reduced.length-1; i >= 0; i--) {
+                reduced[i] = Math.abs((reduced[i + 1] || 0) + reduced[i]) % 10;
+            }
+        }
+        return reduced.slice(0, 8);
+
     },
     inputStrToArr: (str) => {
         return str.split("").map(val => parseInt(val));
@@ -34,27 +39,17 @@ const FFT = {
         if(phases === 0) { return input; }
 
         let output = [];
-
-        let line = '';
         for(let phase = 0; phase < phases; phase++) {
             for(let idx = 0; idx < input.length; idx++) {
-                line = '';
                 let repeated = FFT.buildRepeatingPattern(pattern, idx+1);   
-                // console.log("Repeated: ");
-                // console.log(repeated); 
                 let value = 0;
-                let patternIdx = 1;        
-                for(let col = 0; col < input.length; col++) {
+                let patternIdx = 1+idx;   
+                     
+                for(let col = idx; col < input.length; col++) {
                     value = value + (input[col] * repeated[patternIdx]);
-                    
-                    line = `${line}${input[col]}*${repeated[patternIdx]}`;
-                    if(col<input.length) { line = `${line} + `;}
-                
                     patternIdx++;
                     if(patternIdx >= repeated.length) { patternIdx = 0; }
                 }
-                line = `${line} = ${Math.abs(value)%10}`;
-                // console.log(line);
                 output.push(Math.abs(value)%10);
             }
             input = [...output];
